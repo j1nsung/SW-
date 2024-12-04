2dc00d63e3d1bea1108efcd12842378865ae2f20
jinsung <jinsung9252@naver.com>
2024-12-04 09:26:52 +0900

    Enemy
    적 유닛 생성 및 지정하는 함수

--- 20212322홍진성/Game_Project/main.c
+++ 20212322홍진성/Game_Project/main.c
@@ -6,31 +6,53 @@
 #include <stdbool.h>
 #define _CRT_SECURE_NO_WARNINGS
 
-#define WIDTH 62
-#define HEIGHT 48
-#define UX 30  //시작 유저의 x좌표
-#define UY 45  //시작 유저의 y좌표
+
+
+#define SPECIAL1 0XE0
+#define SPECIAL2 0x00
+#define SPACEBAR 0x20
+#define UP 0x48
+#define DOWN 0x50
+#define LEFT 0x4b
+#define RIGHT 0x4d
+
+
+
+
+#define WIDTH 40
+#define HEIGHT 30
+#define UX 10  //시작 유저의 x좌표
+#define UY 15  //시작 유저의 y좌표
 #define MAXBULLET 6
 #define TRUE 1
 #define FALSE 0
-
-struct 
-{
-	int exist;
-	int x, y;
-}Bullet[MAXBULLET];
-
+#define MAXENERMY 6
 
 int Delay = 10;
 int Frame_Count = 0;
 int P1_Frame_Sync = 4;
 int called = 0;
-int oldx = UX, oldy = UY; // 플레이어의 old 좌표
+int oldx = UX , oldy = UY; // 플레이어의 old 좌표
 int newx = UX, newy = UY; //플레이어의 new 좌표
 int keep_moving = 1;  //1:계속이동
-int x;
-int y;
-unsigned char nKey;
+void Bullet_Move();
+
+struct
+{
+	int exist;
+	int x, y;
+}Bullet[MAXBULLET];
+
+struct
+{
+	int exist;
+	int x, y;
+	int number;
+	int move;
+	int type;
+}Enermy[MAXENERMY];
+
+char* Enermy_Unit[] = { "⊙★⊙","＠▒＠","♨▣♨","◐▼◑" };
 
 void textcolor(int Text_Color, int Background_Color)
 //Text_Color는 글자의 색상
@@ -58,7 +80,7 @@ typedef enum Color { //수업 중에 사용한 컬러 함수를 복붙, 문제시 #define을 이용
 	WHITE,
 }COLOR;
 
-enum Keys { //수업중에 사용한 키보드 함수를 복붙, 문제시 #define을 이용
+/*enum Keys { //수업중에 사용한 키보드 함수를 복붙, 문제시 #define을 이용
 	LEFT = 75,
 	RIGHT = 77,
 	UP = 72,
@@ -66,7 +88,7 @@ enum Keys { //수업중에 사용한 키보드 함수를 복붙, 문제시 #define을 이용
 	SPACEBAR = 32,
 	SPECIAL1 = 0xE0, // 0xE0 값 추가
 	SPECIAL2 = 0x00  // 0x00 값 추가
-};
+};*/
 
 void gotoxy(int x, int y) { //좌표를 지정해주는 함수
 	COORD pos = { x, y };
@@ -74,6 +96,8 @@ void gotoxy(int x, int y) { //좌표를 지정해주는 함수
 	//y는 세로 좌표값
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+
+
 
 void Help() {
 	system("cls"); //화면 지우기
@@ -96,58 +120,61 @@ void removeCursor(void) { //마우스 깜빡거리는 것을 없에는 함수
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 
+void playerdraw(int x, int y) {
+		textcolor(GREEN, BLACK);
+		gotoxy(x, y);
+		printf("<=♥=>"); // 플레이어 출력
+}
+
+
 
 void Init_Game() {
 	system("cls");
 	removeCursor();
+	playerdraw(oldx, oldy);
 }
 
-void playerdraw(int x, int y) {
-	textcolor(GREEN, BLACK);
-	gotoxy(x, y);
-	printf("<<=♥==>");
-}
 
 void playererase(int x, int y) {
 	gotoxy(x, y);
-	printf("   ");
+	printf("      ");
+	fflush(stdout);
 }
 
-void Player_Move(unsigned char Keys) {
+void Player_Move(unsigned char nKey) {
 	int move_flag = 0;
-	static unsigned char last_ch = 0;
 
-
-	if (called == 0) {
-		removeCursor();
-		playerdraw(oldx, oldy);
-		called = 1;
-	}
-	if (keep_moving && Keys == 0)
-		Keys = last_ch;
-	last_ch = Keys;
-
-
-	switch (Keys) {
+	switch (nKey) 
+	{
 	case UP: // 방향키 윗키를 누르면 위로 위치값 변경
-		if (oldy > 25)
+		if (oldy > 25) //화면 상단 제한
+		{
 			newy = oldy - 1;
-		move_flag = 1;
+			move_flag = 1;
+		}
 		break;
 	case DOWN: // 방향키 아랫키를 누르면 아래로 위치값 변경
-		if (oldy < HEIGHT - 3)
+		if (oldy < HEIGHT - 3) //화면 하단 제한
+		{
 			newy = oldy + 1;
-		move_flag = 1;
+			move_flag = 1;
+		}
 		break;
 	case LEFT: // 방향키 왼쪽키를 누르면 왼쪽으로 위치값 변경
-		if (oldx > 2)
+		if (oldx > 2) //화면 좌측 제한
+		{
 			newx = oldx - 1;
-		move_flag = 1;
+			move_flag = 1;
+		}
 		break;
 	case RIGHT: // 방향키 오른쪽키를 누르면 오른쪽으로 위치값 변경
-		if (oldx < WIDTH - 6)
+		if (oldx < WIDTH - 6) //화면 우측 제한
+		{
 			newx = oldx + 1;
-		move_flag = 1;
+			move_flag = 1;
+		}
+		break;
+	default:
 		break;
 	}
 	if (move_flag) {
@@ -165,7 +192,7 @@ void Game_Start() { //게임을 시작 시키는 함수
 	Init_Game(); //재시작시 초기화 해주는 함수
 
 	while (1) {
-		if (_kbhit() == 1) {
+		if (_kbhit()) {
 			nKey = _getch();
 			if (nKey == SPECIAL1 || nKey == SPECIAL2) {
 				nKey = _getch();
@@ -175,16 +202,12 @@ void Game_Start() { //게임을 시작 시키는 함수
 				case LEFT:
 				case RIGHT: //키보드를 이용하여 플레이어 위치 변경
 					Player_Move(nKey);
-					if (Frame_Count % P1_Frame_Sync == 0)
-						Player_Move(0);
 					break;
 				default:
-					if (Frame_Count % P1_Frame_Sync == 0)
-						Player_Move(0);
-
+					break;
 				}
 			}
-			if (nKey == SPACEBAR) {
+			else if (nKey == SPACEBAR) { // 총알 발사
 				for (i = 0; i < MAXBULLET && Bullet[i].exist == TRUE; i++) {}
 				if (i != MAXBULLET) {
 					Bullet[i].x = newx + 1;
@@ -192,11 +215,12 @@ void Game_Start() { //게임을 시작 시키는 함수
 					Bullet[i].exist = TRUE;
 				}
 			}
+			
 		}
-
 		else {
 			//Player_Move 함수 속도 조절
 		}
+		Bullet_Move(); //총알 이동
 		Sleep(Delay); //Delay 값 줄이기
 		Frame_Count++; //Frame_count 값으로 속도 조절
 	}
@@ -232,28 +256,15 @@ void StartMenu() {
 	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 void DrawBullet(int i) {
 	textcolor(GREEN, BLACK);
 	gotoxy(Bullet[i].x, Bullet[i].y);
-	printf(" | ");
+	printf("|");
 }
 
 void EraseBullet(int i) {
 	gotoxy(Bullet[i].x, Bullet[i].y);
-	printf("   ");
+	printf(" ");
 }
 
 void Bullet_Move() {
@@ -272,6 +283,28 @@ void Bullet_Move() {
 			}
 		}
 	}	
+}
+
+void Show_Enermy() {
+	int i, location, direct, height;
+	location = rand() % 2;
+	direct = 5 + rand() % 55;
+	height = 1 + rand() % 8;
+	for(i=0; i < MAXENERMY && Enermy[i].exist==TRUE; i++){}
+	if (i != MAXENERMY) {
+		if (location == 1) {
+			Enermy[i].x = direct;
+			Enermy[i].y = height;
+			Enermy[i].move = 1;
+		}
+		else {
+			Enermy[i].x = direct;
+			Enermy[i].y = height;
+			Enermy[i].move = -1;
+		}
+		Enermy[i].type = rand() % 4;
+		Enermy[i].exist = TRUE;	
+	}
 }
 
 int main() {
